f613a1c33aa36871f2011c6951e4c422aa3cc8f4
RonaldNolascoU <alexnolasco_2010@hotmail.com>
2020-03-19 21:07:12 -0600

    feat types of careers

---
+++ PensumTree/PensumTree/Controllers/TipoCarreraController.cs
@@ -0,0 +1,71 @@
+﻿using PensumTree.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PensumTree.Controllers
+{
+    class TipoCarreraController : Controller<tipo_carrera>
+    {
+
+        public Operation<tipo_carrera> addRecord(tipo_carrera c)
+        {
+            try
+            {
+                EntitySingleton.Context.tipo_carrera.Add(c);
+                EntitySingleton.Context.SaveChanges();
+                return FactoryOperation<tipo_carrera>.getSuccessOperation();
+            }
+            catch (Exception e)
+            {
+                return FactoryOperation<tipo_carrera>.getFailOperation(e.Message);
+            }
+        }
+
+        public Operation<tipo_carrera> deleteRecord(tipo_carrera c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Operation<tipo_carrera> getRecords()
+        {
+            try
+            {
+                List<tipo_carrera> data = EntitySingleton.Context.tipo_carrera.ToList();
+                return FactoryOperation<tipo_carrera>.getDataOperation(data);
+            }
+            catch (Exception e)
+            {
+                return FactoryOperation<tipo_carrera>.getFailOperation(e.Message);
+            }
+        }
+
+        public Operation<tipo_carrera> getActiveRecords()
+        {
+            try
+            {
+                List<tipo_carrera> data = EntitySingleton.Context.tipo_carrera.Where(x => x.estado).ToList();
+                return FactoryOperation<tipo_carrera>.getDataOperation(data);
+            }
+            catch (Exception e)
+            {
+                return FactoryOperation<tipo_carrera>.getFailOperation(e.Message);
+            }
+        }
+
+        public Operation<tipo_carrera> updateRecord(tipo_carrera c)
+        {
+            try
+            {
+                tipo_carrera tipo_carrera = EntitySingleton.Context.tipo_carrera.Find(c.id);
+                EntitySingleton.Context.Entry(tipo_carrera).CurrentValues.SetValues(c);
+                EntitySingleton.Context.SaveChanges();
+                return FactoryOperation<tipo_carrera>.getSuccessOperation();
+            }
+            catch (Exception e)
+            {
+                return FactoryOperation<tipo_carrera>.getFailOperation(e.Message);
+            }
+        }
+    }
+}
--- PensumTree/PensumTree/MantenimientoTipoCarrera.Designer.cs
+++ PensumTree/PensumTree/MantenimientoTipoCarrera.Designer.cs
@@ -28,57 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.components = new System.ComponentModel.Container();
+            this.txtUV = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.comboBox8 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvTipoCarreras = new System.Windows.Forms.DataGridView();
             this.lblTitulo = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menúToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.rdbInactivo = new System.Windows.Forms.RadioButton();
+            this.rdbActivo = new System.Windows.Forms.RadioButton();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoCarreras)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtUV
             // 
-            this.textBox1.Location = new System.Drawing.Point(247, 118);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 139;
-            // 
-            // comboBox5
-            // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(247, 78);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(290, 21);
-            this.comboBox5.TabIndex = 138;
+            this.txtUV.Location = new System.Drawing.Point(247, 118);
+            this.txtUV.Name = "txtUV";
+            this.txtUV.Size = new System.Drawing.Size(100, 20);
+            this.txtUV.TabIndex = 139;
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(678, 142);
+            this.button3.Location = new System.Drawing.Point(678, 102);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(172, 36);
             this.button3.TabIndex = 137;
-            this.button3.Text = "Eliminar";
+            this.button3.Text = "Limpiar";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // comboBox8
-            // 
-            this.comboBox8.FormattingEnabled = true;
-            this.comboBox8.Location = new System.Drawing.Point(467, 157);
-            this.comboBox8.Name = "comboBox8";
-            this.comboBox8.Size = new System.Drawing.Size(192, 21);
-            this.comboBox8.TabIndex = 136;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // label3
             // 
@@ -90,16 +78,6 @@
             this.label3.TabIndex = 135;
             this.label3.Text = "Filtro";
             // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(678, 88);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(172, 36);
-            this.button2.TabIndex = 134;
-            this.button2.Text = "Editar";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -109,14 +87,16 @@
             this.button1.TabIndex = 133;
             this.button1.Text = "Agregar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // dataGridView1
+            // dgvTipoCarreras
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 197);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(860, 329);
-            this.dataGridView1.TabIndex = 132;
+            this.dgvTipoCarreras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTipoCarreras.Location = new System.Drawing.Point(12, 197);
+            this.dgvTipoCarreras.Name = "dgvTipoCarreras";
+            this.dgvTipoCarreras.Size = new System.Drawing.Size(860, 329);
+            this.dgvTipoCarreras.TabIndex = 132;
+            this.dgvTipoCarreras.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
             // 
             // lblTitulo
             // 
@@ -127,14 +107,6 @@
             this.lblTitulo.Size = new System.Drawing.Size(162, 29);
             this.lblTitulo.TabIndex = 131;
             this.lblTitulo.Text = "Tipo de carrera";
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(247, 157);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 130;
             // 
             // label6
             // 
@@ -183,21 +155,64 @@
             this.menúToolStripMenuItem.Text = "Menú";
             this.menúToolStripMenuItem.Click += new System.EventHandler(this.MenúToolStripMenuItem_Click);
             // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(455, 156);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(200, 20);
+            this.txtSearch.TabIndex = 141;
+            this.txtSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox2_KeyPress);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(247, 77);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(308, 20);
+            this.txtName.TabIndex = 142;
+            // 
+            // rdbInactivo
+            // 
+            this.rdbInactivo.AutoSize = true;
+            this.rdbInactivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbInactivo.Location = new System.Drawing.Point(291, 154);
+            this.rdbInactivo.Name = "rdbInactivo";
+            this.rdbInactivo.Size = new System.Drawing.Size(91, 28);
+            this.rdbInactivo.TabIndex = 158;
+            this.rdbInactivo.Text = "Inactivo";
+            this.rdbInactivo.UseVisualStyleBackColor = true;
+            // 
+            // rdbActivo
+            // 
+            this.rdbActivo.AutoSize = true;
+            this.rdbActivo.Checked = true;
+            this.rdbActivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbActivo.Location = new System.Drawing.Point(200, 154);
+            this.rdbActivo.Name = "rdbActivo";
+            this.rdbActivo.Size = new System.Drawing.Size(79, 28);
+            this.rdbActivo.TabIndex = 157;
+            this.rdbActivo.TabStop = true;
+            this.rdbActivo.Text = "Activo";
+            this.rdbActivo.UseVisualStyleBackColor = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
             // MantenimientoTipoCarrera
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 549);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.rdbInactivo);
+            this.Controls.Add(this.rdbActivo);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtUV);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.comboBox8);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvTipoCarreras);
             this.Controls.Add(this.lblTitulo);
-            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label11);
@@ -205,9 +220,12 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MantenimientoTipoCarrera";
             this.Text = "MantenimientoTipoCarrera";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MantenimientoTipoCarrera_FormClosed);
+            this.Load += new System.EventHandler(this.MantenimientoTipoCarrera_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoCarreras)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,20 +233,21 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.TextBox txtUV;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ComboBox comboBox8;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvTipoCarreras;
         private System.Windows.Forms.Label lblTitulo;
-        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem menúToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.RadioButton rdbInactivo;
+        private System.Windows.Forms.RadioButton rdbActivo;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
