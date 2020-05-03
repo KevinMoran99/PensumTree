cea7da981a91e9d34e69684f23fe77e72d2fca0f
RonaldNolascoU <alexnolasco_2010@hotmail.com>
2020-05-03 15:36:31 -0600

    added end user view of pensum

---
+++ PensumTree/PensumTree/ListadoPensums.cs
@@ -0,0 +1,68 @@
+﻿using PensumTree.Controllers;
+using PensumTree.EntityFramework;
+using PensumTree.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PensumTree
+{
+    public partial class ListadoPensums : Form
+    {
+        private static PlanController planContoller = new PlanController();
+        private List<plan> plans = new List<plan>();
+
+        public ListadoPensums()
+        {
+            InitializeComponent();
+        }
+
+        private void loadTable()
+        {
+            int x = 150;
+            int y = 0;
+
+            Operation<plan> getPlans = planContoller.getRecords();
+            if (getPlans.State)
+            {
+                plans = getPlans.Data;
+                foreach (plan plan in plans)
+                {
+                    var control = new PensumControl(plan);
+                    control.Location = new Point(x, y);
+                    panel1.Controls.Add(control);
+                    y += control.Height;
+                    panel1.Height = panel1.Height + 90;
+
+                }
+                return;
+            }
+            MessageBox.Show("Error al cargar los datos de pensums", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+
+        private void ListadoPensums_Load(object sender, EventArgs e)
+        {
+            loadTable();
+        }
+
+        private void ListadoPensums_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnAdminAccess_Click(object sender, EventArgs e)
+        {
+            Login loginForm = new Login();
+            loginForm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ PensumTree/PensumTree/PensumControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PensumTree.Controllers;
+using PensumTree.EntityFramework;
+using PensumTree.Utils;
+
+namespace PensumTree
+{
+    public partial class PensumControl : UserControl
+    {
+        public PensumControl()
+        {
+            InitializeComponent();
+        }
+
+        public PensumControl(plan plan): this()
+        {
+            lblInicio.Text = plan.inicio.ToString();
+            lblFin.Text = plan.fin.ToString();
+            chbEstado.Checked = plan.estado;
+            chbEstado.Text = plan.estado ? "Activo" : "Inactivo";
+            lblCarrera.Text = plan.carrera.ToString();
+        }
+    }
+}
